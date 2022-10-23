c2b7209a041a83c8cdbb263e38416e2d7dcc86a4
BennyDagur <bdg2@nemandi.ntv.is>
2022-10-23 13:40:18 +0000

    check

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html","/public/programming-list-page.html", "/public/programming-details-page.html",
+    "./index.html","./public/programming-list-page.html", "./public/programming-details-page.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
